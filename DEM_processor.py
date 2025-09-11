a9bf44a0dfd826efd4e4db02754ba8ea2c8b5db8
LEE SANGYUN <speedp001@naver.com>
2025-09-11 20:34:46 +0900

    feat: Expand DEM coverage and enable selective region processing

--- DEM_processor.py
+++ DEM_processor.py
@@ -1,4 +1,3 @@
-import os
 import rasterio
 import numpy as np
 import open3d as o3d
@@ -15,24 +14,25 @@ from open3d.visualization import VisualizerWithKeyCallback
 ##### DEMProcessor 클래스 정의
 class DEMProcessor:
 
-    def __init__(self, lat, lon, dem_path, bin_path, mesh_path, upsample):
+    def __init__(self, lat, lon, dem_path, bin_path, mesh_path, upsample, visualization):
         self.lat = lat
         self.lon = lon
         self.dem_path = dem_path
         self.bin_path = bin_path
         self.mesh_path = mesh_path
         self.upsample = upsample
-        
+        self.visualization = visualization
+
     ##### GPS → UTM 좌표 변환 및 범위 검사
     def gps_to_utm(self, lat, lon):
         """
         lat: GPS 위도
         lon: GPS 경도
         """
-        
+
         # GPS → UTM 좌표 변환기
         transformer = Transformer.from_crs("EPSG:4326", "EPSG:5179", always_xy=True)
-        
+
         # GPS 좌표 → UTM 좌표
         x_utm, y_utm = transformer.transform(lon, lat)
 
@@ -48,7 +48,7 @@ class DEMProcessor:
         # print(f"GPS -> UTM: x={x_utm}, y={y_utm}")
 
         return x_utm, y_utm
-    
+
     def get_altitude(self):
         """
         DEM에서 주어진 GPS 좌표의 고도 값을 반환
@@ -67,7 +67,7 @@ class DEMProcessor:
             # UTM -> Pixel 역행렬(~transform)을 이용하여 좌표 변환
             col_f, row_f = (~transform) * (x_utm, y_utm)
             h, w = dem.shape
-            
+
             # 주변 4개 점 인덱스
             c0, r0 = int(np.floor(col_f)), int(np.floor(row_f))
             c1, r1 = c0 + 1, r0 + 1
@@ -89,13 +89,13 @@ class DEMProcessor:
             return float(z_utm)
 
     ##### DEM 데이터를 읽고 BIN 파일로 변환
-    def make_bin(self, radius, visualization):
+    def make_bin(self, radius):
         """
         DEM 데이터를 읽고 BIN 파일로 변환
         GPS 기준 반경 radius(m) DEM만 BIN 파일로 변환
         radius: DEM을 읽을 범위 (m)
         """
-        
+
         # GPS 좌표 -> UTM 좌표
         x_utm, y_utm = self.gps_to_utm(self.lat, self.lon)
 
@@ -134,21 +134,21 @@ class DEMProcessor:
             (x_utm_interp, y_utm_interp),
             method='cubic'
         )
-        
+
         # 최종 3D 포인트 병합 (utm x, utm y, dem z)
         pts = np.stack((x_utm_interp, y_utm_interp, z_utm_interp), axis=2)
         pts = pts.reshape(-1, 3).astype(np.float32)
-        
+
         # NaN 값 제거
         # 거리 기준으로 뽑으면 원형 형태로 나오므로, grid 크기로 업샘플링하면 사격형 형태이므로 NaN 값이 생길 수 있음
         pts = pts[~np.isnan(pts[:, 2])]
-        
+
         # BIN 저장
         pts.tofile(self.bin_path)
         print(f"Saved {self.bin_path}")
 
         # DEM 시각화
-        if (visualization==True):
+        if (self.visualization==True):
             # 포인트 클라우드 생성
             tree = KDTree(pts[:, :2])
             query_pt = self.gps_to_utm(self.lat, self.lon)
@@ -166,6 +166,6 @@ class DEMProcessor:
             ctr.set_lookat(center_pt)     # 카메라 시선 목표점 (x,y,z)
             ctr.set_front([0, 0, 1])      # 카메라 앞 방향 벡터
             ctr.set_up([0, 1, 0])         # 카메라 위 방향 벡터
-            
+
             vis.run()
             vis.destroy_window()
