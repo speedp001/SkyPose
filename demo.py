9e972cc5ecfeabf4cfa5abf8e30b476678a85e30
LEE SANGYUN <speedp001@naver.com>
2025-09-29 21:11:34 +0900

    feat: Add demo.py

--- demo.py
+++ demo.py
@@ -11,12 +11,12 @@ from skyline_matcher import SkylineMatcher
 # FOV_H = 36.5
 
 # Galaxy S23 FOV
-FOV_V = 73.7
-FOV_H = 57.6
+FOV_V = 48
+FOV_H = 74
 
 LAT = 37.55453
 LON = 127.04611
-SAMPLE_STEP = 20
+SAMPLE_STEP = 10
 
 DEM_PATH  = "./skyline_db/korea_DEM.img"
 BIN_PATH = "./skyline_db/korea_DEM.bin"
@@ -28,19 +28,19 @@ IMAGE_PATH = "./client_data/37.55453_127.04611/image.png"
 
 
 ##### 카메라 기울기 및 고도 각도 -> IMU센서 이용해서 추정 예정
-YAW = 180
-PITCH = 5
-ROLL = 33
+YAW = 0
+PITCH = 0
+ROLL = 0
 
 
 
 
 
 ##### DEMProcessor
-dem = DEMProcessor(LAT, LON, DEM_PATH, BIN_PATH, MESH_PATH, upsample=1, visualization=False)
+dem = DEMProcessor(LAT, LON, DEM_PATH, BIN_PATH, MESH_PATH, upsample=1, visualization=True)
 
 # BIN 파일 생성 및 시각화
-dem.make_bin(radius=50000)
+# dem.make_bin(radius=50000)
 
 # Altitude 조회
 # altitude = dem.get_altitude()
@@ -62,7 +62,7 @@ pixel_angles, user_skyline, seg_base64 = extractor.user_skyline(SAMPLE_STEP, PIT
 dem_skyline = extractor.skyline_360_DEM(LAT, LON, pixel_angles)
 
 # Real-time 360도 skyline 추출
-# dem_skyline = extractor.real_time_skyline_360_DEM(LAT, LON, pixel_angles)
+dem_skyline = extractor.real_time_skyline_360_DEM(LAT, LON)
 
 ##### SkylineMatcher
 matcher = SkylineMatcher(IMAGE_PATH, FOV_V, FOV_H, YAW, SAMPLE_STEP, search_radius=30, visualization=True)
